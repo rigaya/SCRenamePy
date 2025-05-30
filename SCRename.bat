69f4d09eab73951a898654c7058aa232cbce50a4
rigaya <rigaya34589@live.jp>
2025-05-30 21:30:32 +0900

    SCRename.pyに合わせてバッチファイルを修正。

--- SCRename.bat
+++ SCRename.bat
@@ -1,11 +1,11 @@
 @echo off
 setlocal
 
-rem SCRename.vbs をインストールしたパスを設定
+rem SCRename.py をインストールしたパスを設定
 set SCRPATH=%~dp0
 
 :LOOP
-for /F "usebackq delims=" %%I in (`cscript //nologo "%SCRPATH%\SCRename.vbs" "%~1" "$SCtitle$ $SCpart$第$SCnumber$話 「$SCsubtitle$」 ($SCservice$)"`) do set SCRTARGET=%%~I
+for /F "usebackq delims=" %%I in (`py "%SCRPATH%\SCRename.py" "%~1" "$SCtitle$ $SCpart$第$SCnumber$話 「$SCsubtitle$」 ($SCservice$)"`) do set SCRTARGET=%%~I
 
 rem 設定例
 rem call encode.bat "%SCRTARGET%"
--- SCRenameEDCB.bat
+++ SCRenameEDCB.bat
@@ -1,10 +1,10 @@
 @echo off
 setlocal
 
-rem SCRename.vbs をインストールしたパスを設定
+rem SCRename.py をインストールしたパスを設定
 set SCRPATH=C:\SCRename
 
-for /F "usebackq delims=" %%I in (`cscript //nologo "%SCRPATH%\SCRename.vbs" "$FilePath$" "$SCtitle$ $SCpart$第$SCnumber$話 「$SCsubtitle$」 ($SCservice$)" %2 %3`) do set SCRTARGET=%%~I
+for /F "usebackq delims=" %%I in (`py "%SCRPATH%\SCRename.py" "$FilePath$" "$SCtitle$ $SCpart$第$SCnumber$話 「$SCsubtitle$」 ($SCservice$)" %2 %3`) do set SCRTARGET=%%~I
 
 rem 設定例
 rem call encode.bat "%SCRTARGET%"
